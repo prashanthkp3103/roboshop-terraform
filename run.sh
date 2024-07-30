b91c265a10abd0cba2167e03ebe53fa2411e318d
prasanthbangs2016 <prasanthbangs@gmail.com>
2024-07-30 14:53:36 +0530

    terraform

--- run.sh
+++ run.sh
@@ -1,2 +1,13 @@
+if [ $# -ne 2 ]; then
+  echo $0 env(dev|prod) action(apply|destroy)
+  exit 1
+fi
+
 git pull
 rm -rf .terraform
+terraform init -backend-config=env-${1}/state.tfvars
+terraform $2 -var-file=env-${1}/main.tfvars --auto-approve
+
+#bash run.sh dev apply
+#bash run.sh prod apply
+
