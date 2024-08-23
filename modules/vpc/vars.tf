b167673738a7695e4fa9f3982fd6fe92f8b2a098
prasanthbangs2016 <prasanthbangs@gmail.com>
2024-08-23 12:53:53 +0530

    vpc peering

--- modules/vpc/vars.tf
+++ modules/vpc/vars.tf
@@ -1,10 +1,10 @@
 variable "cidr" {}
 variable "env" {}
-# variable "public_subnets" {}
-# variable "web_subnets" {}
-# variable "app_subnets" {}
-# variable "db_subnets" {}
-# variable "availability_zones" {}
-# variable "default_vpc_id" {}
-# variable "default_vpc_rt" {}
-# variable "default_vpc_cidr" {}
+variable "public_subnets" {}
+variable "web_subnets" {}
+variable "app_subnets" {}
+variable "db_subnets" {}
+variable "availability_zones" {}
+variable "default_vpc_id" {}
+variable "default_vpc_rt" {}
+variable "default_vpc_cidr" {}
--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "vpc" {}
 variable "env" {}
-variable "default_vpc_id" {}
-variable "default_vpc_rt" {}
-variable "default_vpc_cidr" {}
+# variable "default_vpc_id" {}
+# variable "default_vpc_rt" {}
+# variable "default_vpc_cidr" {}
 
