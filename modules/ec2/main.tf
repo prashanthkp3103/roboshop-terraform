b1890dadb13dcd4264d845e9f51899a450308471
prasanthbangs2016 <prasanthbangs@gmail.com>
2024-08-28 07:19:03 +0530

    added asg

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -49,4 +49,9 @@ resource "aws_autoscaling_group" "main" {
     id      = aws_launch_template.main.id
     version = "$Latest"
   }
+  tag {
+    key                 = "Name"
+    propagate_at_launch = true
+    value               = "${var.name}-${var.env}"
+  }
 }
