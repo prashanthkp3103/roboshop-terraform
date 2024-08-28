6fdf66a5c130ca89e9e3c2d715c638e264ca3c4c
prasanthbangs2016 <prasanthbangs@gmail.com>
2024-08-28 06:48:15 +0530

    added asg launch template

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -28,3 +28,35 @@ resource "aws_security_group" "allow_tls" {
     Name = "${var.name}-${var.env}"
   }
 }
+
+resource "aws_launch_template" "main" {
+  name = "${var.name}-${var.env}"
+  image_id      = data.aws_ami.ami.id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+}
+
+# resource "aws_autoscaling_group" "example" {
+#   availability_zones = ["us-east-1a"]
+#   desired_capacity   = 1
+#   max_size           = 1
+#   min_size           = 1
+#
+#   mixed_instances_policy {
+#     launch_template {
+#       launch_template_specification {
+#         launch_template_id = aws_launch_template.example.id
+#       }
+#
+#       override {
+#         instance_type     = "c4.large"
+#         weighted_capacity = "3"
+#       }
+#
+#       override {
+#         instance_type     = "c3.large"
+#         weighted_capacity = "2"
+#       }
+#     }
+#   }
+# }
