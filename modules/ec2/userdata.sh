75ce8e42f2d5e74a9f14e1ee40dcb993b165afee
prasanthbangs2016 <prasanthbangs@gmail.com>
2024-08-28 10:36:02 +0530

    user data added for ansible roles to run

--- modules/ec2/userdata.sh
+++ modules/ec2/userdata.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-pip3.11 install ansible 2>&1 | tee -a /opt/userdata.log
+#installs ansible and hashicorp valut packages
+pip3.11 install ansible hvac 2>&1 | tee -a /opt/userdata.log
+#note shell variables are accessed with ${}
 ansible-pull -i localhost, -U https://github.com/prashanthkp3103/Latest-Ansible-Roboshop main.yml -e env=${env} -e role_name=${role_name} -e vault_token=${vault_token} 2>&1 | tee -a /opt/userdata.log
