f8f0072dd7b35f4d398d0964eb54cbf63bc2de5e
prasanthbangs2016 <prasanthbangs@gmail.com>
2024-09-02 12:43:30 +0530

    updated user data

--- modules/ec2/userdata.sh
+++ modules/ec2/userdata.sh
@@ -2,5 +2,4 @@
 
 #installs ansible and hashicorp valut packages
 pip3.11 install ansible hvac 2>&1 | tee -a /opt/userdata.log
-#note shell variables are accessed with ${}
 ansible-pull -i localhost, -U https://github.com/prashanthkp3103/Latest-Ansible-Roboshop main.yml -e env=${env} -e role_name=${role_name} -e vault_token=${vault_token} 2>&1 | tee -a /opt/userdata.log
