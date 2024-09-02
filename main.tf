4f57a1dd3c3d6278b64473463c8654a7ea524b02
prasanthbangs2016 <prasanthbangs@gmail.com>
2024-09-02 12:38:47 +0530

    added dependency for app module

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ module "vpc" {
 }
 
 module "apps" {
-  depends_on = [module.db]
+  depends_on = [module.db, module.vpc]
   source = "./modules/ec2"
 
   for_each = var.apps
@@ -43,6 +43,7 @@ output "web" {
 }
 
 module "db" {
+  depends_on = [module.vpc]
   source = "./modules/ec2"
 
   for_each = var.db
