ffc4a31e13c335567dd9c9df1975e7ad35bd66fe
prasanthbangs2016 <prasanthbangs@gmail.com>
2024-09-04 14:47:19 +0530

    added all app components

--- modules/ec2/userdata.sh
+++ modules/ec2/userdata.sh
@@ -2,4 +2,4 @@
 
 #installs ansible and hashicorp valut packages
 pip3.11 install ansible hvac 2>&1 | tee -a /opt/userdata.log
-ansible-pull -i localhost, -U https://github.com/prashanthkp3103/Latest-Ansible-Roboshop main.yml -e env=${env} -e role_name=${role_name} -e vault_token=${vault_token} 2>&1 | tee -a /opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/prasanthbangs2016/raghu-roboshop-ansible main.yml -e env=${env} -e role_name=${role_name} -e vault_token=${vault_token} 2>&1 | tee -a /opt/userdata.log
