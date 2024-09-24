ba8b703e3e190ddc1fa95b1347e02dc1500086de
prasanthbangs2016 <prasanthbangs@gmail.com>
2024-09-24 11:14:46 +0530

    updated

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -203,8 +203,9 @@ resource "aws_lb_listener" "lb_listener" {
 ##
 
 #creates multiple records based asg variable true or false
+#cname is alias name for lb
 resource "aws_route53_record" "lb" {
-  count = var.asg ? 1 : 0 #if var.asg is true then 1(create) else 0(dont create) - 0 false -1 true
+  count = var.asg ? 1 : 0 #create if var.asg is true(created) then 1(create) else 0(dont create) - 0 false -1 true
   zone_id = var.zone_id
   name    = "${var.name}.${var.env}"
   type    = "CNAME"
